d8d72fbcea2a61c76e2722ed966fcc236d95eef6
hjkim <mihaelkel@naver.com>
2021-05-05 23:20:07 +0900

    add stm32f1xx rcc register in stm32f1xx_rcc.h

---
+++ stm32f1xx_rcc.h
@@ -0,0 +1,20 @@
+#define RCC_BASE_ADDRESS        0x40021000
+
+#pragma pack(push ,1)
+
+//refernce manual 121 page
+typedef struct {
+	uint32_t CR;
+	uint32_t CFGR;
+	uint32_t CIR;
+	uint32_t APB2RSTR;
+	uint32_t APB1RSTR;
+	uint32_t AHBENR;
+	uint32_t APB2ENR;
+	uint32_t APB1ENR;
+	uint32_t BDCR;
+	uint32_t CSR;
+}RCC_t;
+
+
+#pragma pack(pop)
